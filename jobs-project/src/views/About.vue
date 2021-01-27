9486cd3f18cf13a45516d9c37e99a381260a9292
mcorcias <mosescorcias@gmail.com>
2021-01-28 00:13:57 +0200

    Router basic

---
+++ jobs-project/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptates officia laudantium dicta alias minus, nihil vitae animi in quas ad id eligendi dolore itaque. Maxime non similique corrupti quasi nam quis earum quo sed reiciendis? Hic ab inventore id voluptas, rerum nisi animi excepturi nemo debitis facere labore non magni dignissimos pariatur nobis, culpa odio! Saepe molestiae magni pariatur ex perspiciatis eius repellat! Placeat magni eaque nemo. Corrupti eligendi et recusandae ducimus vel dolorum, laboriosam repellat praesentium sunt, autem natus facilis hic sequi dignissimos aliquam doloribus veniam ex modi. Laborum distinctio, eum labore vero fugit similique laboriosam id non!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptates officia laudantium dicta alias minus, nihil vitae animi in quas ad id eligendi dolore itaque. Maxime non similique corrupti quasi nam quis earum quo sed reiciendis? Hic ab inventore id voluptas, rerum nisi animi excepturi nemo debitis facere labore non magni dignissimos pariatur nobis, culpa odio! Saepe molestiae magni pariatur ex perspiciatis eius repellat! Placeat magni eaque nemo. Corrupti eligendi et recusandae ducimus vel dolorum, laboriosam repellat praesentium sunt, autem natus facilis hic sequi dignissimos aliquam doloribus veniam ex modi. Laborum distinctio, eum labore vero fugit similique laboriosam id non!</p>
+
+  </div>
+</template>
---
+++ jobs-project/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab consequuntur, molestias quos tenetur beatae, eum dicta voluptas recusandae facilis iusto modi error fuga repudiandae impedit! Est facere, quia culpa minus adipisci quidem quae ipsum deleniti quasi aliquid in soluta. Ratione consequuntur nam quia tenetur earum obcaecati, harum beatae qui cumque consectetur aspernatur ipsa cupiditate corrupti ipsam dignissimos nostrum libero architecto, quis, inventore optio ut expedita incidunt ex! Cumque, laboriosam laudantium quae eaque officiis, suscipit vitae veniam aliquid quo aperiam similique nisi unde mollitia? Veritatis quod nobis nihil minima beatae quam deleniti? Consequatur asperiores, odio explicabo cum debitis aspernatur consectetur laborum.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab consequuntur, molestias quos tenetur beatae, eum dicta voluptas recusandae facilis iusto modi error fuga repudiandae impedit! Est facere, quia culpa minus adipisci quidem quae ipsum deleniti quasi aliquid in soluta. Ratione consequuntur nam quia tenetur earum obcaecati, harum beatae qui cumque consectetur aspernatur ipsa cupiditate corrupti ipsam dignissimos nostrum libero architecto, quis, inventore optio ut expedita incidunt ex! Cumque, laboriosam laudantium quae eaque officiis, suscipit vitae veniam aliquid quo aperiam similique nisi unde mollitia? Veritatis quod nobis nihil minima beatae quam deleniti? Consequatur asperiores, odio explicabo cum debitis aspernatur consectetur laborum.</p>
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
