73c2e414bdf11f4029a0219b5eb9cc1947dfaf02
Moshe Corcias <mosheco@B-COM.INTERNAL>
2021-01-28 13:19:29 +0200

    installes dammi databas - using in json-server

--- jobs-project/src/views/jobs/Jobs.vue
+++ jobs-project/src/views/jobs/Jobs.vue
@@ -1,23 +1,32 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-      <router-link :to="{name: 'JobDetails',params:{id:job.id}}">
-        <h2>{{job.title}}</h2>
-      </router-link>
+  <div v-if="this.jobs.length>0">
+       <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{name: 'JobDetails',params:{id:job.id}}">
+                <h2>{{job.title}}</h2>
+            </router-link>
+        </div>
   </div>
+  <div v-else>
+     <h1>Loading job details...</h1>
+  </div>
+ 
 </template>
 
 <script>
 export default {
     data(){
         return{
-            jobs:[
-                {title:'UX Designer', id:1, details:'lorem'},
-                {title:'Web Developer', id:2, details:'lorem'},
-                {title:'Vue Developer',id:3, details:'lorem'},
-            ]
+            jobs:[]
         }
+    },
+    mounted(){
+        fetch('http://localhost:3000/jobs')
+        .then((res)=>res.json())
+        .then(data=>this.jobs=data)
+        .catch(err=>console.log(err.message))
     }
+    
 }
 </script>
 
